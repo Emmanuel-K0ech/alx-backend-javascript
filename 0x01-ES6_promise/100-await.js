252abf64b43f1fbc7c54fabfc340fa6cf7b66833
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-03 01:00:01 +0300

    Task advanced : including try / catch statements

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,8 +3,12 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   let result = {};
 
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  result = { photo, user };
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
+  } catch (error) {
+    result = { photo: null, user: null };
+  }
   return result;
 }
