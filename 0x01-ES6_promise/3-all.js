afa97774f157007066cc910d4f102643840ac143
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-02 23:13:27 +0300

    Task 3: Using Promise.all to resolve all the promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((upload) => createUser().then((result) => {
-      console.log(`${upload.body} ${result.firstName} ${result.lastName}`);
-    }))
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
