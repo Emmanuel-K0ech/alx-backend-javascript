46920235e2b3b75cedaa2e28ddb4d87f0ca32262
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-10-03 21:55:31 +0300

    Task 1: MakeClassRoom fucntion

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
-export default class ClassRoom {
-    constructor(maxStudentSize) {
-        this._maxStudentSize = maxStudentSize;
-    }
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
